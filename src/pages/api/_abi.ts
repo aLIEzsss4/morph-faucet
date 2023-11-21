a3cb9e4949da81db04205316ef35d929b3d085a1
aLIEzsss4 <vuetomato@gmail.com>
2023-11-22 03:41:21 +0800

    feat: api

---
+++ src/pages/api/_abi.ts
@@ -0,0 +1,145 @@
+export const wagmiAbi = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_claimIntervalHours",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "claimant",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "claimTime",
+        type: "uint256",
+      },
+    ],
+    name: "Claim",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "deposit",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address payable",
+        name: "recipient",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "sendFunds",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "newIntervalHours",
+        type: "uint256",
+      },
+    ],
+    name: "updateClaimInterval",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdrawAll",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+  {
+    inputs: [],
+    name: "getBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getClaimInterval",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "lastAccessTime",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
---
+++ src/pages/api/sendFunds.ts
@@ -0,0 +1,95 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import {
+  createWalletClient,
+  http,
+  parseEther,
+  defineChain,
+  isAddress,
+} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { wagmiAbi } from "./_abi";
+
+export const redstoneHoleSky = defineChain({
+  id: 17001,
+  name: "Redstone HoleSky",
+  network: "holesky",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.holesky.redstone.xyz"],
+      webSocket: ["wss://rpc.holesky.redstone.xyz/ws"],
+    },
+    public: {
+      http: ["https://rpc.holesky.redstone.xyz"],
+      webSocket: ["wss://rpc.holesky.redstone.xyz/ws"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://explorer.holesky.redstone.xyz" },
+  },
+  contracts: {
+    contract: {
+      address: "0xE36D7af621241a91B89D22d0ffEE0C464A67f7FC",
+      blockCreated: 649879,
+    },
+  },
+});
+
+const walletClient = createWalletClient({
+  chain: redstoneHoleSky,
+  transport: http(),
+});
+
+const account = privateKeyToAccount(process.env.OWNER_PRIVATE_KEY as Address);
+
+type Data = {
+  message: string;
+  url?: string;
+};
+
+type Address = `0x${string}`;
+
+const amount = parseEther("0.0000002");
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const recipient: Address = req.query.address as Address;
+
+  if (recipient === undefined) {
+    res.status(400).json({
+      message: "Address is undefined!",
+    });
+  } else if (!isAddress(recipient)) {
+    res.status(400).json({
+      message: "Invalid address!",
+    });
+  }
+
+  try {
+    const tx = await walletClient.writeContract({
+      address: redstoneHoleSky.contracts.contract.address,
+      abi: wagmiAbi,
+      functionName: "sendFunds",
+      args: [recipient, amount],
+      account,
+    });
+    console.log(tx);
+    res
+      .status(200)
+      .json({
+        message: "Success!",
+        url: redstoneHoleSky.blockExplorers.default.url + "/tx/" + tx,
+      });
+  } catch (error) {
+    res.status(400).json({
+      message: (error as any).details as string,
+    });
+  }
+}
